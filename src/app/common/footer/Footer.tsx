fc1bd0791ca72d91fa8f66cb7f7007ccd2b090f0
Cezar <cezar.toma1@gmail.com>
2020-12-02 17:29:26 +0200

    footer completed

--- src/app/common/footer/Footer.tsx
+++ src/app/common/footer/Footer.tsx
@@ -1,44 +1,84 @@
-import { Flex, Icon, Text } from "@chakra-ui/react";
-import Link from 'next/link';
-import FacebookIcon from '../../../../public/svg/FacebookIcon.svg';
-import LinkedInIcon from '../../../../public/svg/LinkedInIcon.svg';
-import InstagramIcon from '../../../../public/svg/InstagramIcon.svg';
-
+import { Flex, Icon, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import FacebookIcon from '../../../../public/svg/FacebookIcon.svg'
+import LinkedInIcon from '../../../../public/svg/LinkedInIcon.svg'
+import InstagramIcon from '../../../../public/svg/InstagramIcon.svg'
+import CegoltarLogo from '../../../../public/svg/cegoltar-footer-logo.svg'
 
 export default function Footer() {
   return (
-    <Flex as="footer" background="primary.500" height="300px" pt="46px" justifyContent="center">
-      <Flex width="1133px" flexDir='column'>
-        <Flex className='footer-container' flexDir='row'>
-          <Flex className='footer-info' flexDir="column" color="white">
-            <Text fontSize="large" mb="32px">
-              Informatii
-            </Text>
-            <Link href="/about-us">Despre noi</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
+    <Flex
+      as="footer"
+      background="#124193"
+      height="349px"
+      pt="46px"
+      pb="46px"
+      justifyContent="center"
+    >
+      <Flex flexDir="column" width="100%" mr="163px" ml="154px">
+        <Flex className="footer-container" flexDir="row">
+          <Flex className="footer-info-container" width="480px">
+            <Flex className="footer-info" flexDir="column">
+              <Text fontSize="24px" mb="19px">
+                Informatii
+              </Text>
+              <Link href="/about-us">Despre noi</Link>
+              <Link href="/">Condiții de garanție</Link>
+              <Link href="/">Magazine regionale</Link>
+              <Link href="/">Livrare</Link>
+              <Link href="/">Termeni și condiții</Link>
+            </Flex>
+            <Flex className="footer-info" flexDir="column">
+              <Text fontSize="24px" mb="19px">
+                Informatii
+              </Text>
+              <Link href="/about-us">Despre noi</Link>
+              <Link href="/">Condiții de garanție</Link>
+              <Link href="/">Magazine regionale</Link>
+              <Link href="/">Livrare</Link>
+              <Link href="/">Termeni și condiții</Link>
+            </Flex>
           </Flex>
-          <Flex flexDir="column" color="white">
-            <Text fontSize="large" mb="32px">
-              Informatii
+          <Flex direction="column" alignSelf="start" align="start">
+            <Text noOfLines={2} className="contact-info">
+              <Flex direction="column">
+                <span>Call centru:</span>
+                <span> +373 69 606 707</span>
+              </Flex>
             </Text>
-            <Link href="/about-us">Despre noi</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
-            <Link href="/">Conditii De Garantie</Link>
+            <Icon as={CegoltarLogo} height="52px" width="256px" />
           </Flex>
         </Flex>
-        <hr className='footer-hr'/>
-        <Flex flexDir='row' justifyContent='space-between' className='footer-copy-div'>
-          <Flex flexDir='row'>
-            <Icon className='footer-icons' as={FacebookIcon}/>
-            <Icon className='footer-icons' as={InstagramIcon}/>
-            <Icon className='footer-icons' as={LinkedInIcon}/>
+
+        <hr className="footer-hr" />
+
+        <Flex
+          flexDir="row"
+          justifyContent="space-between"
+          className="footer-copyright-div"
+          mb="25px"
+        >
+          <Flex flexDir="row">
+            <Icon
+              className="footer-icons"
+              as={FacebookIcon}
+              width="16px"
+              height="16px"
+            />
+            <Icon
+              className="footer-icons"
+              as={InstagramIcon}
+              width="16px"
+              height="16px"
+            />
+            <Icon
+              className="footer-icons"
+              as={LinkedInIcon}
+              width="16px"
+              height="16px"
+            />
           </Flex>
-          <Text className='footer-copywright' fontSize='12px' color="gray" fontWeight='400'>
+          <Text className="footer-copywright">
             Copywright &copy; 2020 Cegoltar S.R.L. str. Petricani, 21 MD-2005
           </Text>
         </Flex>
--- src/app/common/header/SubHeader.tsx
+++ src/app/common/header/SubHeader.tsx
@@ -1,16 +1,29 @@
-import { useQuery } from "@apollo/client";
-import { Box, Flex, Icon, Popover, PopoverContent, PopoverTrigger, Text } from "@chakra-ui/react";
-import Link from "next/link";
+import { useQuery } from '@apollo/client'
+import {
+  Box,
+  Flex,
+  Icon,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+  Text,
+} from '@chakra-ui/react'
+import Link from 'next/link'
 import MenuIcon from '../../../../public/svg/MenuIcon.svg'
-import { IProductCategoriesData, ProductCategoriesQuery } from "../../app-features/categories/ProductCategoriesQueries";
+import {
+  IProductCategoriesData,
+  ProductCategoriesQuery,
+} from '../../app-features/categories/ProductCategoriesQueries'
 
 export default function SubHeader(): JSX.Element {
-
-
-  const { loading, error, data } = useQuery<IProductCategoriesData>(ProductCategoriesQuery)
+  const { loading, error, data } = useQuery<IProductCategoriesData>(
+    ProductCategoriesQuery
+  )
   console.log(data)
-  const rootProductCategories = data?.productCategories.filter(cat => cat.parent === null);
-  console.log(data?.productCategories);
+  const rootProductCategories = data?.productCategories.filter(
+    (cat) => cat.parent === null
+  )
+  console.log(data?.productCategories)
 
   return (
     <Flex
@@ -32,13 +45,12 @@ export default function SubHeader(): JSX.Element {
               color="white"
             >
               <Icon as={MenuIcon} />
-              <Text fontWeight="medium">
-                Catalogul Produselor
-              </Text>
+              <Text fontWeight="medium">Catalogul Produselor</Text>
             </Flex>
           </PopoverTrigger>
           <PopoverContent h="300px" p="20px">
-            {
+            {/* Eroare la compilare */}
+            {/* {
               loading
                 ? <div>Loading...</div>
                 : rootProductCategories.map(category =>
@@ -48,14 +60,11 @@ export default function SubHeader(): JSX.Element {
                     </Flex>
                   </Link>
                 )
-            }
+            } */}
           </PopoverContent>
         </Popover>
       </Flex>
-      <Flex
-        justify='center'
-        align='center'
-      >
+      <Flex justify="center" align="center">
         <Text>Magazine Regionale</Text>
         <Text>Service Centru</Text>
         <Text>&#350;tiri</Text>
