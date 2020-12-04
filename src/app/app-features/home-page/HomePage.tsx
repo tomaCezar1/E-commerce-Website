92973a48ec0b2ba3baf637eb5f37e6737934d633
Mishanea <mihigh.ciobanu@gmail.com>
2020-12-04 17:53:45 +0200

    Move commits from dev

--- src/app/app-features/home-page/HomePage.tsx
+++ src/app/app-features/home-page/HomePage.tsx
@@ -3,7 +3,7 @@ import CarouselComponent from './CarouselComponent/CarouselComponent'
 
 export default function HomePage(): JSX.Element {
   return (
-    <Flex maxW="1152px" w="100%">
+    <Flex maxW="1152px" w="100%" id="homepage">
       <h1>Home Page</h1>
       <CarouselComponent />
     </Flex>
