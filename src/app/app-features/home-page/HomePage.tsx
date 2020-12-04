c4fb202066766d3592b7dbe3fae9e6e425b51d34
bashmentarium <mihigh.ciobanu@gmail.com>
2020-12-04 13:24:45 +0200

    Add styles and room for test

--- src/app/app-features/home-page/HomePage.tsx
+++ src/app/app-features/home-page/HomePage.tsx
@@ -2,7 +2,7 @@ import { Flex } from "@chakra-ui/react";
 
 export default function HomePage(): JSX.Element {
   return (
-    <Flex maxW="1133px" w="100%">
+    <Flex maxW="1152px" w="100%" height="2500px" backgroundColor='#b3b3b3'>
       <h1>Home Page</h1>
     </Flex>
   )
