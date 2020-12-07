5c5f562df5e354aab5b3507176312ddb2ec0d600
Cezar <cezar.toma1@gmail.com>
2020-12-07 21:58:41 +0200

    product card created

---
+++ src/app/app-features/home-page/product-card/ProductCard.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+function ProductCard() {
+  return (
+    <>
+      <Container></Container>
+    </>
+  )
+}
+
+export default ProductCard
+
+const Container = styled.div`
+  width: 248px;
+  height: 390px;
+`
