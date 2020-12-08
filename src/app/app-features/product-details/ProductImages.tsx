6963160f812e4fcbea908a666e82cd8f9af5656d
Cezar <cezar.toma1@gmail.com>
2020-12-08 16:51:32 +0200

    fixed issues

--- src/app/app-features/product-details/ProductImages.tsx
+++ src/app/app-features/product-details/ProductImages.tsx
@@ -1,49 +1,33 @@
 import { Skeleton } from '@chakra-ui/react'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 
 function ProductImages(props) {
   // const { loading, error, data } = useQuery<ICarouselItems>(CarouselQuery)
   // const items = data?.carouselItems
 
-  const [currentImg, setCurrentImg] = useState(null)
-  const [clicked, setClicked] = useState(true)
-  const isClicked = !clicked
+  const [currentImg, setCurrentImg] = useState(0)
 
-  useEffect(() => {
-    setCurrentImg(props.images[0])
-  }, [props])
-
-  const setImg = (event, index) => {
+  const toggleClick = (index) => {
     setCurrentImg(index)
-    setClicked(isClicked)
-
-    if (event.target.className === 'small-img small-img-active') {
-      event.target.className = 'small-img'
-      console.log('removed')
-    } else {
-      event.target.className = 'small-img small-img-active'
-      console.log('added class')
-    }
   }
 
   return (
     <>
       <div>
-        <img className="big-image" src={currentImg} />
+        <img className="big-image" src={props.images[currentImg]} />
       </div>
       <div className="small-img-container">
-        {/* <Skeleton isLoaded={!loading} h="100%" w="100%"> */}
+        {/* <Skeleton h="100%" w="100%"> */}
         {props.images.map((img, index) => (
           <img
-            onClick={
-              // () => setCurrentImg(props.images[index])
-              () => setImg(event, props.images[index])
-            }
-            // className={isClicked ? `small-img small-img-active ` : `small-img`}
-            className="small-img"
+            onClick={() => toggleClick(index)}
+            className={`small-img ${
+              currentImg === index ? 'small-img-active' : ''
+            }`}
             key={index}
             src={img}
             alt="error"
+            data-index={index}
           />
         ))}
         {/* </Skeleton> */}
