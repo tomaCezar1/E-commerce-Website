62d9030eda2ddc96e2f23bd470ad5391ac2f03a0
Cezar <cezar.toma1@gmail.com>
2020-12-09 11:49:28 +0200

    created task-59

---
+++ src/app/app-features/home-page/product-list/ProductList.tsx
@@ -0,0 +1,12 @@
+function ProductList(): JSX.Element {
+  return (
+    <>
+      <div className="product-list-container">
+        <h1>Produse Cegoltar</h1>
+        <div className="cards-container"></div>
+      </div>
+    </>
+  )
+}
+
+export default ProductList
