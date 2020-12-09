e286fdb0b1a5fdfeccdd5d8b9e35fe8d3c80aaf1
Mishanea <mihigh.ciobanu@gmail.com>
2020-12-09 12:11:28 +0200

    Show categories along with children

--- src/app/common/menu/Menu.tsx
+++ src/app/common/menu/Menu.tsx
@@ -1,46 +1,52 @@
 import React, { useState, useContext, useEffect } from 'react'
 import {Skeleton} from "@chakra-ui/react";
+import {sortCategories } from '../../../utils/categories'
 import { AppContext } from '../../../context'
 
 export default function Menu(): JSX.Element {
-  const [subCategories, showSubCategories] = useState(false)
   const { appContext } = useContext(AppContext)
+  const [categories, setCategories] = useState([])
+  const [showSubCategories, setShowSubCategories] = useState(false)
 
   useEffect(() => {
-    console.log(appContext);
+    setCategories(sortCategories(appContext.categories))
   }, [])
 
-  const toggleMouseEnter = () => {
-    showSubCategories(!subCategories)
+  const toggleMouse = () => {
+    setShowSubCategories(!showSubCategories)
   }
+
 
   return (
     <div className="menu-container">
-      <div className="categories-list">
-        {
-          appContext.categories?.map((cat, i) => {
-            return <div key={i}>{cat.title}</div>
-          })
-        }
-        {/* {categories.map((category) => {
-          const {title, id, parent} = category
-          if(parent === null) {
-            return (
-              <div
-                className="menu-list-item"
-                key={id}
-                onMouseEnter={toggleMouseEnter}
-                onMouseLeave={toggleMouseEnter}
-              >
-                <span className="menu-item-title">{title}</span>
-              </div>
-            )
-          }
-        })} */}
-      </div>
-      <div className="subcategories">
+      <ul className="categories-list">
+        {categories.map((category) => {
+          const {title, id, children} = category
+          return (
+            <li
+              className="menu-list-item"
+              key={id}
+              onMouseEnter={toggleMouse}
+              onMouseLeave={toggleMouse}
+            >
+              <span className="menu-item-title">{title}</span>
+              {children && showSubCategories && (
+                <ul className="subcategories">
+                  {children.map(element => {
+                    const {id, title} = element
 
-      </div>
+                    return (
+                      <li className="children-item" key={id}>
+                        <span className="children-title">{title}</span>
+                      </li> 
+                    )
+                  })}
+              </ul>
+              )}
+            </li>
+          )
+        })}
+      </ul>
     </div>
   )
 }
