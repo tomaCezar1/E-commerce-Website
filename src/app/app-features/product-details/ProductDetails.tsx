97ae06e255877fb0b4d177f257ff70b106f04579
Cezar <cezar.toma1@gmail.com>
2020-12-09 18:57:08 +0200

    first part

--- src/app/app-features/product-details/ProductDetails.tsx
+++ src/app/app-features/product-details/ProductDetails.tsx
@@ -1,4 +1,5 @@
 import ProductImages from './ProductImages'
+import CartIcon from '../../../../public/svg/CartIcon.svg'
 
 function ProductDetails() {
   const product1 = {
@@ -17,7 +18,41 @@ function ProductDetails() {
 
   return (
     <>
-      <ProductImages images={product1.images} />
+      <div className="product-details-container">
+        <div className="product-details-flex">
+          <div className="product-details-slideshow">
+            <ProductImages images={product1.images} />
+          </div>
+          <div className="product-details-content">
+            <h1 className="product-details-title">
+              Bec LED 11W 2700K (E27)Bec LED 11W 2700K (E27)
+            </h1>
+            <div className="product-details-info">
+              <p className="product-details-in-stock">Produsul este în stoc</p>
+              <p className="product-details-serial-nb">SKU D551AI</p>
+            </div>
+            <p className="product-details-text">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vitae
+              vestibulum penatibus velit dignissim amet, facilisis turpis
+              fermentum integer. Non integer consectetur pretium tellus. Tortor,
+              proin at maecenas proin turpis velit vestibulum tortor sed.
+            </p>
+            <a className="product-details-detalii">Mai multe detalii</a>
+            <div className="product-details-cart-price">
+              <h1 className="product-details-price">50 lei</h1>
+              <div className="product-details-add-to-cart">
+                <CartIcon />
+                <p className="product-details-add-text">Adaugă în coș</p>
+              </div>
+            </div>
+            <div className="product-details-counter">
+              <button>-</button>
+              <p>1</p>
+              <button>+</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
