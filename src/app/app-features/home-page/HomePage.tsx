eef2c042f620fe02f0bd55f78242bd6a5fec3f6e
Mishanea <mihigh.ciobanu@gmail.com>
2020-12-10 13:51:08 +0200

    Remove styled component

--- src/app/app-features/home-page/HomePage.tsx
+++ src/app/app-features/home-page/HomePage.tsx
@@ -1,26 +1,17 @@
-import styled from 'styled-components'
 import CarouselComponent from './CarouselComponent/CarouselComponent'
 import PartnerList from './PartnerList/PartnerList'
 import ProductCard from './product-card/ProductCard'
 
 export default function HomePage(): JSX.Element {
   return (
-    <Div id="homepage">
+    <div id="homepage">
       <h1>Home Page</h1>
       <CarouselComponent />
       <div style={{ height: '5rem' }}></div>
-      <ProductCard/>
+      <ProductCard />
       <div style={{ height: '5rem' }}></div>
       <PartnerList />
       <div style={{ height: '5rem' }}></div>
-    </Div>
+    </div>
   )
 }
-
-const Div = styled.div`
-  max-width: 1152px;
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-`
