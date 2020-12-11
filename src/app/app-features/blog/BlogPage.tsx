9e70b072fb8b280a864524f0f5ff0849fa26938f
Cezar <cezar.toma1@gmail.com>
2020-12-11 12:26:42 +0200

    created task-67

---
+++ src/app/app-features/blog/BlogPage.tsx
@@ -0,0 +1,12 @@
+function BlogPage(): JSX.Element {
+  return (
+    <>
+      <div>
+        <h1>Știri</h1>
+        <div className="blog-page-container"></div>
+      </div>
+    </>
+  )
+}
+
+export default BlogPage
