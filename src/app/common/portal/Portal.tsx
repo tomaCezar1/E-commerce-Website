875d938aa823ccc5eb8fd990058937fb253ce8e3
Mishanea <mihigh.ciobanu@gmail.com>
2020-12-14 17:57:40 +0200

    Add overlay Portal and icons

---
+++ src/app/common/portal/Portal.tsx
@@ -0,0 +1,46 @@
+import ReactDOM from 'react-dom';
+import { useEffect } from "react";
+
+export function PortalOverlay({children, onBackdropClick}) {
+  const portalWrapper = document.createElement('div');
+  portalWrapper.classList.add('portal-wrapper')
+
+  useEffect(() => {
+    document.body.appendChild(portalWrapper);
+    portalWrapper.addEventListener('click', (e) => {
+      if (e.target.classList.contains('portal-wrapper')){
+        onBackdropClick(e)
+      }
+    })
+
+    const searchContainer = document.getElementById('Search')
+
+    searchContainer.addEventListener('click', () => searchContainer.focus())
+
+    portalWrapper.addEventListener('wheel', checkScrollDirection);
+
+    function checkScrollDirection(event) {
+      if (checkScrollDirectionIsUp(event)) {
+        // Direction up
+      } else {
+        if (event.target.classList.contains('portal-wrapper')){
+          onBackdropClick(event)
+          searchContainer.blur()
+        }
+      }
+    }
+
+    function checkScrollDirectionIsUp(event) {
+      if (event.wheelDelta) {
+        return event.wheelDelta > 0;
+      }
+      return event.deltaY < 0;
+    }
+
+    return () => {
+      document.body.removeChild(portalWrapper);
+    }
+  },[children])
+
+  return ReactDOM.createPortal(children, portalWrapper)
+}
