ae77a564ad154370497a6a726a85f84424c6eebe
Cezar <cezar.toma1@gmail.com>
2020-12-18 17:40:34 +0200

    bugs issues

--- src/app/app-features/home-page/HomePage.tsx
+++ src/app/app-features/home-page/HomePage.tsx
@@ -1,19 +1,13 @@
 import CarouselComponent from "./CarouselComponent/CarouselComponent";
 import PartnerList from "./PartnerList/PartnerList";
-import ProductCard from "./product-card/ProductCard";
 import ProductList from "./product-list/ProductList";
 
 export default function HomePage({ homePageInfo }): JSX.Element {
   return (
     <div>
       <CarouselComponent style={{ marginTop: "34px" }} />
-      <div style={{ marginTop: "40px" }} className="title-1">
-        Produsele Cegoltar
-      </div>
-      <div style={{ height: "5rem" }}></div>
-      <PartnerList />
-      <div style={{ height: "5rem" }}></div>
       <ProductList homePageInfo={homePageInfo} />
+      <PartnerList />
     </div>
   );
 }
--- src/app/app-features/home-page/product-list/ProductList.tsx
+++ src/app/app-features/home-page/product-list/ProductList.tsx
@@ -5,7 +5,7 @@ function ProductList({ homePageInfo }): JSX.Element {
   const items = homePageInfo.products;
   return (
     <div className="product-list-container">
-      <h1 className="product-list-header">Produse Cegoltar</h1>
+      <h1 className="product-list-header">Produsele Cegoltar</h1>
       <div className="cards-container">
         {items?.map((item, index) => {
           return <ProductCard product={item} key={index} />;
