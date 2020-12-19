5175bcd094d8b19657e41c080ab72abaafe4b2b4
Mishanea <mihigh.ciobanu@gmail.com>
2020-12-19 13:11:20 +0200

    Fix form styles

--- src/app/app-features/cart-page/CheckoutForm/CheckoutForm.tsx
+++ src/app/app-features/cart-page/CheckoutForm/CheckoutForm.tsx
@@ -1,17 +1,19 @@
-import { useState } from "react"
-import Link from "next/link"
-import InputMask from 'react-input-mask';
+import { useState } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import InputMask from "react-input-mask";
 
 const initialValues = {
-  name: '',
-  phone: '',
-  callTime: ''
-}
+  name: "",
+  phone: "",
+  callTime: "",
+};
 
-export default function CheckoutForm({validate}): JSX.Element {
-  const [values, setValues] = useState(initialValues)
+export default function CheckoutForm({ validate }): JSX.Element {
+  const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState({});
   const [touched, setTouched] = useState({});
+  const router = useRouter();
 
   const handleChange = (event) => {
     const { name, value: newValue, type } = event.target;
@@ -19,15 +21,15 @@ export default function CheckoutForm({validate}): JSX.Element {
     const value = type === "number" ? +newValue : newValue;
     setValues({
       ...values,
-      [name]: value
+      [name]: value,
     });
     setTouched({
       ...touched,
-      [name]: true
+      [name]: true,
     });
-  }
+  };
 
-  const handleBlur = event => {
+  const handleBlur = (event) => {
     const { name, value } = event.target;
 
     const { [name]: removedError, ...rest } = errors;
@@ -37,31 +39,32 @@ export default function CheckoutForm({validate}): JSX.Element {
     // // validate the field if the value has been touched
     setErrors({
       ...rest,
-      ...(error && { [name]: touched[name] && error })
+      ...(error && { [name]: touched[name] && error }),
     });
   };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const formValidation = Object.keys(values).reduce(
       (acc, key) => {
         const newError = validate[key](values[key]);
         const newTouched = { [key]: true };
+
         return {
           errors: {
             ...acc.errors,
-            ...(newError && { [key]: newError })
+            ...(newError && { [key]: newError }),
           },
           touched: {
             ...acc.touched,
-            ...newTouched
-          }
+            ...newTouched,
+          },
         };
       },
       {
         errors: { ...errors },
-        touched: { ...touched }
+        touched: { ...touched },
       }
     );
     setErrors(formValidation.errors);
@@ -71,9 +74,8 @@ export default function CheckoutForm({validate}): JSX.Element {
       !Object.values(formValidation.errors).length && // Errors object is empty
       Object.values(formValidation.touched).length ===
         Object.values(values).length && // All fields were touched
-      Object.values(formValidation.touched).every(t => t === true) // Every touched field is true
+      Object.values(formValidation.touched).every((t) => t === true) // Every touched field is true
     ) {
-
       // Make the request to place an order instead of the alert
       alert(JSON.stringify(values, null, 2));
     }
@@ -86,54 +88,66 @@ export default function CheckoutForm({validate}): JSX.Element {
         <input
           value={values.name}
           name="name"
-          required
           onChange={handleChange}
           onBlur={handleBlur}
           placeholder="Nume"
-          className={touched.name && errors.name ? "checkout-form-error-input" : "checkout-form-input"}
+          className={
+            touched.name && errors.name
+              ? "checkout-form-error-input"
+              : "checkout-form-input"
+          }
         />
         {touched.name && errors.name ? (
-          <div className="checkout-error">
-            {touched.name && errors.name}
-          </div>
+          <div className="checkout-error">{touched.name && errors.name}</div>
         ) : (
-          <div className="checkout-error-invis">
-            Asd
-          </div>
+          <div className="checkout-error-invis">Asd</div>
         )}
         <InputMask
           mask="+373 99 999 999"
           maskChar={null}
           value={values.phone}
           name="phone"
-          required
           onChange={handleChange}
           onBlur={handleBlur}
           placeholder="Telefon"
-          className={touched.phone && errors.phone ? "checkout-form-error-input" : "checkout-form-input"}
+          className={
+            touched.phone && errors.phone
+              ? "checkout-form-error-input"
+              : "checkout-form-input"
+          }
         />
         {touched.phone && errors.phone ? (
-          <div className="checkout-error">
-            {touched.phone && errors.phone}
-          </div>
+          <div className="checkout-error">{touched.phone && errors.phone}</div>
         ) : (
-          <div className="checkout-error-invis">
-            Asd
-          </div>
+          <div className="checkout-error-invis">Asd</div>
         )}
         <select
           name="callTime"
           onChange={handleChange}
           onBlur={handleBlur}
           value={values.callTime}
-          className={touched.callTime && errors.callTime ? "checkout-form-error-input" : "checkout-form-input"}
-          required
+          className={
+            touched.callTime && errors.callTime
+              ? "checkout-form-error-input"
+              : "checkout-form-input"
+          }
         >
-          <option value="" disabled hidden>Ora apelului</option>
-          <option value="9:00 - 12:00" className="checkout-option-text">9:00 - 12:00</option>
-          <option value="12:00 - 15:00" className="checkout-option-text">12:00 - 15:00</option>
-          <option value="15:00 - 18:00" className="checkout-option-text">15:00 - 18:00</option>
-          <option value="Cît de curînd posibil" className="checkout-option-text">
+          <option value="" disabled hidden>
+            Ora apelului
+          </option>
+          <option value="9:00 - 12:00" className="checkout-option-text">
+            9:00 - 12:00
+          </option>
+          <option value="12:00 - 15:00" className="checkout-option-text">
+            12:00 - 15:00
+          </option>
+          <option value="15:00 - 18:00" className="checkout-option-text">
+            15:00 - 18:00
+          </option>
+          <option
+            value="Cît de curînd posibil"
+            className="checkout-option-text"
+          >
             Cît de curînd posibil
           </option>
         </select>
@@ -142,33 +156,25 @@ export default function CheckoutForm({validate}): JSX.Element {
             {touched.callTime && errors.callTime}
           </div>
         ) : (
-          <div className="checkout-error-invis">
-            Asd
-          </div>
+          <div className="checkout-error-invis">Asd</div>
         )}
-        <input
-          type="submit"
-          value="Comandă"
-          className="checkout-form-button"
-        />
+        <input type="submit" value="Comandă" className="checkout-form-button" />
 
         <div className="checkout-terms-text">
-          *Apăsînd butonul Comandă dvs. confirmați că ați 
-          luat cunoștință și sînteți de acord cu{" "}
-          <Link href="/">
+          *Apăsînd butonul &#39;Comandă&#39;  dvs. confirmați că ați luat cunoștință și
+          sînteți de acord cu{" "}
+          <Link href={`/privacy`} as={`/privacy-policy`} locale={router.locale}>
             <span className="checkout-privacy-link">
               politica de confidențialitate
             </span>
           </Link>
-            <span>{" "}și{" "}</span>
-          <Link href="/">
-            <span className="checkout-privacy-link">
-              termenii de utilizare
-            </span>
+          <span> și </span>
+          <Link href={`/terms`} as={`/terms-and-conditions`} locale={router.locale}>
+            <span className="checkout-privacy-link">termenii de utilizare</span>
           </Link>
           .
         </div>
       </form>
     </div>
-  )
+  );
 }
