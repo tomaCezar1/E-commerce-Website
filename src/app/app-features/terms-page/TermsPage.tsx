39564c6bb81cb6dd6a95b2c9ff95b21b452c3892
Cezar <cezar.toma1@gmail.com>
2020-12-23 15:41:52 +0200

    terms page

--- src/app/app-features/terms-page/TermsPage.tsx
+++ src/app/app-features/terms-page/TermsPage.tsx
@@ -1,10 +1,351 @@
-import Breadcrumbs from '../../common/breadcrumbs/Breadcrumbs'
+import Breadcrumbs from '../../common/breadcrumbs/Breadcrumbs';
 
 export default function TermsPage() {
   return (
     <>
       <Breadcrumbs />
-      <div>Terms and conditions</div>
+      <div className="guarantees-container">
+        <h2 className="terms-page-name">Termenii de utilizare</h2>
+
+        <h4 className="terms-heading">1. DISPOZIȚII GENERALE</h4>
+        <p>
+          1.1. Acest Acord de Utilizare (denumit în continuare „Acord”) se
+          referă la site-ul magazinului online INGCO cu adresa www.ingco.md și
+          la toate site-urile relevante legate de www.ingco.md
+          <br />
+          1.2. Site-ul magazinului online „INGCO” (denumit în continuare -
+          Site-ul) este proprietatea Companiei cu răspundere limitată “Cegoltar
+          SRL”.
+          <br />
+          1.3. Prezentul Acord reglementează relația dintre Administrația
+          site-ului magazinului online „INGCO” (denumit în continuare
+          Administrația site-ului) și Utilizatorul acestui Site.
+          <br />
+          1.4. Administrația site-ului își rezervă dreptul de a modifica, adăuga
+          sau șterge clauzele prezentului Acord în orice moment, fără a notifica
+          Utilizatorul.
+          <br />
+          1.5. Utilizarea continuă a site-ului de către utilizator înseamnă
+          acceptarea acordului și modificările aduse prezentului acord.
+          <br />
+          1.6. Utilizatorul este responsabil personal pentru verificarea
+          prezentului acord pentru modificările aduse de acesta.
+        </p>
+
+        <h4 className="terms-heading">2. DEFINIȚIILE TERMENILOR</h4>
+        <p>
+          2.1. Termenii enumerați mai jos au următoarele semnificații în sensul
+          prezentului acord:
+          <br />
+          2.1.1 'INGCO' - un magazin online situat pe numele de domeniu
+          www.ingco.md, care operează prin resursa Internet și serviciile
+          conexe.
+          <br />
+          2.1.2. Magazin online - un site care conține informații despre Bunuri,
+          Vânzător, care vă permite să selectați, să comandați și (sau) să
+          cumpărați bunurile.
+          <br />
+          2.1.3. Administrarea site-ului web al magazinului online - angajații
+          autorizați să administreze Site-ul, acționând în numele “Cegoltar
+          SRL”.
+          <br />
+          2.1.4. Utilizatorul site-ului web al magazinului online (denumit în
+          continuare Utilizator) este o persoană care are acces la site prin
+          internet și utilizează site-ul.
+          <br />
+          2.1.5. Conținutul site-ului web al magazinului online (denumit în
+          continuare „Conținut”) reprezintă proprietatea intelectuală, inclusiv
+          textele operelor literare, titlurile lor, cuvintele cheie, adnotările,
+          articolele, ilustrațiile, copertele, lucrările muzicale cu sau fără
+          text, grafic, text, fotografii, instrumente derivate, componente și
+          alte lucrări, interfețe de utilizator, interfețe vizuale, numele
+          mărcilor comerciale, logo-urile, programele de calculator, baze de
+          date, precum și proiectarea, structura, selecția, coordonarea,
+          aspectul, stilul general și aranjarea conținutului, care face parte
+          din Site și altele obiecte de proprietate intelectuală conținute
+          colectiv și / sau separat pe site-ul magazinului online.
+        </p>
+
+        <h4 className="terms-heading">3. SUBIECTUL ACORDULUI</h4>
+        <p>
+          3.1. Obiectul prezentului Acord este de a oferi Utilizatorului
+          Magazinului Online acces la Produse și servicii conținute pe Site.
+          <br />
+          3.1.1. Magazinul online oferă Utilizatorului următoarele tipuri de
+          servicii:
+          <ul className="terms-list">
+            <li className="terms-list-item">
+              Acces la conținut electronic în mod gratuit, cu drept de cumpărare
+              (descărcare), vizualizare de conținut.
+            </li>
+            <li className="terms-list-item">
+              Acces la instrumentele de căutare și navigare ale magazinului
+              online.
+            </li>
+            <li className="terms-list-item">
+              Oferirea utilizatorului posibilitatea de a posta mesaje,
+              comentarii, recenzii ale utilizatorului, evaluarea conținutului
+              magazinului online.
+            </li>
+            <li className="terms-list-item">
+              Acces la conținut electronic în mod gratuit, cu drept de cumpărare
+              (descărcare), vizualizare de conținut.
+            </li>
+            <li className="terms-list-item">
+              Acces la informații despre bunuri și informații despre cumpărarea
+              mărfurilor în mod gratuit.
+            </li>
+            <li className="terms-list-item">
+              Alte tipuri de servicii , vândute pe paginile magazinului online.
+            </li>
+          </ul>
+          <br />
+          3.1.2. Prezentul Acord acoperă toate serviciile existente (funcționând
+          efectiv) ale magazinului de internet în acest moment, precum și orice
+          modificări ulterioare și servicii suplimentare ale magazinului de
+          internet care apar în viitor.
+          <br />
+          3.2. Accesul la magazinul online este oferit gratuit.
+          <br />
+          3.3. Prin accesarea site-ului, se consideră că utilizatorul a aderat
+          la prezentul acord.
+          <br />
+          3.4. Utilizarea materialelor și serviciilor site-ului este
+          reglementată de normele actuale ale legislației Republicii Moldova.
+        </p>
+
+        <h4 className="terms-heading">4. DREPTURILE ȘI OBLIGAȚIILE PĂRȚILOR</h4>
+        <p>
+          4.1. Administrația site-ului are dreptul de a:
+          <br />
+          4.1.1. Modifica regulile de utilizare a Site-ului, precum și
+          modificați conținutul acestui site. Modificările intră în vigoare din
+          momentul publicării noii versiuni a acordului pe site.
+          <br />
+          4.1.2. Restricționa accesul la Site în cazul în care Utilizatorul
+          încalcă condițiile prezentului Acord.
+          <br />
+          4.2. Utilizatorul are dreptul de a:4.2.1. Obține acces pentru a
+          utiliza Site-ul.4.2.2. Utiliza toate serviciile disponibile pe Site,
+          precum și achiziționa orice bunuri oferite pe Site.
+          <br />
+          4.2.3. Pune orice întrebare legată de serviciile magazinului online
+          pentru detaliile care se află în secțiunea Site-ului „Telefoane de
+          contact”
+          <br />
+          4.2.4. Utilizarea site-ul numai în scopurile și în modul prevăzut de
+          acord și care nu sunt interzise de legislația Republicii Moldova.
+          <br />
+          4.3. Utilizatorul site-ului se angajează:
+          <br />
+          4.3.1. Să furnizeze, la solicitarea Administrației site-ului,
+          informații suplimentare care sunt direct legate de serviciile
+          furnizate de acest site.
+          <br />
+          4.3.2. Să respecte drepturile de proprietate și non-proprietate ale
+          autorilor și ale altor deținători de drepturi atunci când utilizează
+          Site-ul.
+          <br />
+          4.3.3. Să nu intreprindă acțiuni care pot fi considerate ca
+          perturbează funcționarea normală a Site-ului.
+          <br />
+          4.3.4. Să nu distribuie informații confidențiale și protejate de
+          legislația Republicii Moldova despre persoane fizice sau juridice care
+          utilizează Site-ul.
+          <br />
+          4.3.5. Să evite orice acțiuni care pot încălca confidențialitatea
+          informațiilor protejate de legislația Republicii Moldova.
+          <br />
+          4.3.6. Să nu folosească site-ul pentru a distribui informații
+          publicitare, cu excepția acordului Administrației site-ului.
+          <br />
+          4.3.7. Să nu utilizeze serviciile site-ului magazinului online în
+          scopul:
+          <br />
+          4.3.7. 1. încărcarea de conținut ilegal încalcă orice drepturi ale
+          terților; promovează violența, cruzimea, ura și (sau) discriminarea pe
+          motive rasiale, etnice, de gen, religioase, sociale; conține
+          informații false și (sau) insulte către anumite persoane, organizații,
+          autorități.
+          <br />
+          4.3.7. 2. încurajării de a comite acțiuni ilegale, precum și asistență
+          persoanelor ale căror acțiuni au ca scop încălcarea restricțiilor și
+          interdicțiilor în vigoare pe teritoriul Republicii Moldova.
+          <br />
+          4.3.7. 3. încălcarea drepturilor minorilor și (sau) provocarea
+          acestora în orice formă.
+          <br />
+          4.3.7. 4. încălcarea drepturilor minorităților.
+          <br />
+          4.3.7. 5.prezentării ca o altă persoană sau reprezentant al unei
+          organizații și (sau) comunități fără drepturi suficiente, inclusiv
+          pentru angajații acestui magazin online.
+          <br />
+          4.3.7. 6. înșelăciune cu privire la proprietățile și caracteristicile
+          oricărui Produs din catalogul magazinului online postat pe Site.
+          <br />
+          4.3.7. 7. compararării incorectă a bunurilor, precum și formarea unei
+          atitudini negative față de persoanele (care nu) folosesc anumite
+          bunuri sau condamnarea unor astfel de persoane.
+          <br />
+          4.4. Utilizatorului i se interzice:
+          <br />
+          4.4.1. Utilizarea oricăror dispozitive, programe, proceduri, algoritmi
+          și metode, dispozitive automate sau procese manuale echivalente pentru
+          a accesa, achiziționa, copia sau urmări conținutul site-ului acestui
+          magazin online;
+          <br />
+          4.4.2. Perturbarea a bunei functionare a site-ului;
+          <br />
+          4.4.3. În orice fel, ocoliți structura de navigare a Site-ului pentru
+          a primi sau încerca să obțineți orice informații, documente sau
+          materiale prin orice mijloace care nu sunt furnizate în mod specific
+          de serviciile acestui Site;
+          <br />
+          4.4.4. Acces neautorizat la funcțiile Site-ului, la orice alte sisteme
+          sau rețele legate de acest Site, precum și la orice servicii oferite
+          pe Site;
+          <br />
+          4.4.5. Încălcarea sistemului de securitate de pe Site sau în orice
+          rețea legată de Site;
+          <br />
+          4.4.6. Efectuarea căutării inversă, urmăririi sau încercării de a
+          urmări orice informație despre orice alt utilizator al site-ului.
+          <br />
+          4.4.7. Utilizarea Site-ul și Conținutul său în orice scop interzis de
+          legislația Republicii Moldova, precum și a incita orice activitate
+          ilegală sau altă activitate care încalcă drepturile magazinului online
+          sau ale altor persoane.
+          <br />
+          5. UTILIZAREA SITE-ului DE MAGAZIN ONLINE
+        </p>
+
+        <h4 className="terms-heading">
+          5.1. Site-ul și Conținutul care face parte din Site este deținut și
+          administrat de Administrația Site-ului.
+        </h4>
+        <p>
+          5.2. Conținutul Site-ului nu poate fi copiat, publicat, reprodus,
+          transmis sau distribuit în niciun fel, precum și postat pe internetul
+          global fără acordul prealabil scris al Administrației Site-ului.
+          <br />
+          5.3. Conținutul Site-ului este protejat de drepturile de autor, legea
+          mărcilor comerciale, precum și alte drepturi legate de proprietatea
+          intelectuală și legile concurenței neloiale.
+          <br />
+          5.4. Cumpărarea bunurilor oferite pe site poate necesita crearea unui
+          cont de utilizator.
+          <br />
+          5.5. Utilizatorul este responsabil pentru menținerea
+          confidențialității informațiilor despre cont, inclusiv parola, precum
+          și pentru toate, fără excepție, activitățile desfășurate în numele
+          Utilizatorului contului.
+          <br />
+          5.6. Utilizatorul trebuie să notifice imediat Administrația site-ului
+          orice utilizare neautorizată a contului sau a parolei sale sau a
+          oricărei alte încălcări de securitate.
+          <br />
+          5.7. Administrația site-ului are dreptul să anuleze unilateral contul
+          Utilizatorului, dacă nu a fost utilizat mai mult de 12 luni
+          calendaristice consecutive fără a notifica Utilizatorul.
+          <br />
+          5.8. Prezentul acord se aplică tuturor termenilor și condițiilor
+          suplimentare pentru achiziționarea de bunuri și prestarea de servicii
+          furnizate pe site.
+          <br />
+          5.9. Informațiile postate pe site nu ar trebui interpretate ca o
+          modificare a prezentului acord.
+          <br />
+          5.10. Administrația site-ului are dreptul, în orice moment, fără să
+          notifice Utilizatorul, să efectueze modificări în lista Produselor și
+          serviciilor oferite pe Site și (sau) la prețurile aplicabile acestor
+          Produse pentru vânzarea lor și (sau) serviciile furnizate de magazinul
+          de internet.
+          <br />
+          5.11. Documentele specificate în clauzele 5.11.1 - 5.11.4 din
+          prezentul acord sunt reglementate în partea relevantă și se aplică
+          utilizării site-ului de către utilizator. Prezentul Acord include
+          următoarele documente:
+          <br />
+          5.11.1. Politica de Confidențialitate;
+          <br />
+          5.11.2. Contract de achiziție de la distanță pentru mărfuri;
+          <br />
+          5.11.3. Cerere de comandă;
+          <br />
+          5.11.4. Sugestii și comentarii.
+          <br />
+          5.12. Oricare dintre documentele enumerate în clauza 5.11 din
+          prezentul acord pot fi supuse reînnoirii. Modificările intră în
+          vigoare din momentul publicării pe site.
+          <br />
+          6. RESPONSABILITATE
+          <br />
+          6.1. Orice pierderi pe care Utilizatorul le poate suferi în cazul
+          încălcării intenționate sau imprudente a oricărei prevederi a
+          prezentului Acord, precum și din cauza accesului neautorizat la
+          comunicațiile altui Utilizator, nu sunt rambursate de către
+          Administrația site-ului.
+        </p>
+
+        <h4 className="terms-heading">
+          6.2. Administrația site-ului nu este responsabilă pentru:
+        </h4>
+        <p>
+          6.2.1. Întârzieri sau defecțiuni în procesul de efectuare a unei
+          operațiuni cauzate de forța majoră, precum și orice caz de defecțiuni
+          în telecomunicații, computer, electrică și alte sisteme conexe.
+          <br />
+          6.2.2. Acțiuni ale sistemelor de transfer, bănci, sisteme de plată și
+          întârzieri asociate activității lor.
+          <br />
+          6.2.3. Funcționarea corespunzătoare a Site-ului, în cazul în care
+          Utilizatorul nu dispune de mijloacele tehnice necesare pentru a-l
+          utiliza și, de asemenea, nu poartă nicio obligație de a oferi
+          utilizatorilor astfel de mijloace.
+          <br />
+          7. VIOLAREA TERMENILOR ACORDULUI DE UTILIZATOR
+          <br />
+          7.1. Administrația site-ului are dreptul să dezvăluie orice informație
+          colectată despre Utilizatorul acestui Site, în cazul în care
+          divulgarea este necesară în legătură cu o investigație sau o
+          reclamație cu privire la utilizarea necorespunzătoare a Site-ului sau
+          pentru a stabili (identifica) un Utilizator care poate încălca sau
+          interfera cu drepturile Administrației Site-ului sau a drepturilor
+          altor Utilizatori ai Site-ului.
+        </p>
+
+        <h4 className="terms-heading">
+          7.2. Administrația site-ului are dreptul să dezvăluie orice informație
+          despre Utilizator pe care o consideră necesară pentru a respecta
+          prevederile legislației actuale sau a hotărârilor judecătorești,
+          pentru a asigura respectarea condițiilor prezentului Acord, pentru a
+          proteja drepturile sau siguranța Cegoltar SRL.
+        </h4>
+        <p>
+          7.3. Administrația site-ului are dreptul să dezvăluie informații
+          despre Utilizator dacă legislația actuală a Republicii Moldova impune
+          sau permite o astfel de divulgare.
+          <br />
+          7.4. Administrația site-ului are dreptul să încheie și (sau) să
+          blocheze accesul la Site fără o notificare prealabilă a
+          Utilizatorului, dacă Utilizatorul a încălcat prezentul Acord sau
+          condițiile de utilizare a Site-ului conținute în alte documente,
+          precum și în cazul încetării Site-ului sau din cauza unei probleme
+          tehnice sau a unei probleme.
+          <br />
+          7.5. Administrarea site-ului nu este responsabilă de Utilizator sau de
+          terți pentru încetarea accesului la Site în cazul încălcării de către
+          Utilizator a oricărei prevederi din prezentul Acord sau a unui alt
+          document care conține condițiile de utilizare a Site-ului.
+          <br />
+          8. SOLUȚIONAREA LITIGIILOR
+          <br />
+          8.1. În caz de dezacord sau dispută între părțile prezentului acord, o
+          condiție prealabilă înainte de a merge în instanță este să depună o
+          cerere (o propunere scrisă pentru soluționarea ameabila a litigiului).
+        </p>
+      </div>
     </>
-  )
+  );
 }
