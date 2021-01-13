995e1b75a06059fd4ba95c89112d9408cb37886c
Mishanea <mihigh.ciobanu@gmail.com>
2021-01-13 12:13:58 +0200

    Add lang switch to mobile

--- src/app/common/header/LangSwitch/LangSwitch.tsx
+++ src/app/common/header/LangSwitch/LangSwitch.tsx
@@ -1,21 +1,31 @@
-import React from 'react'
-import { useRouter } from 'next/router'
+import React from 'react';
+import { useRouter } from 'next/router';
 
-export default function LangSwitch(): JSX.Element {
-  const router = useRouter()
+export default function LangSwitch({ mobile }): JSX.Element {
+  const router = useRouter();
 
   const handleClick = (lang: string) => {
-    router.push('/', '/', {locale: lang})
-  }
+    router.push('/', '/', { locale: lang });
+  };
 
   return (
-    <div className={`lang-wrapper ${router.locale === 'ro' ? 'ro-active' : 'ru-active'}`}>
-      <div className={router.locale === 'ro' ? "lang active" : 'lang'} onClick={() => handleClick('ro')}>
+    <div
+      className={`lang-wrapper ${mobile && 'mobile'} ${
+        router.locale === 'ro' ? 'ro-active' : 'ru-active'
+      }`}
+    >
+      <div
+        className={router.locale === 'ro' ? 'lang active' : 'lang'}
+        onClick={() => handleClick('ro')}
+      >
         Ro
       </div>
-      <div className={router.locale === 'ru' ? "lang active" : 'lang'}  onClick={() => handleClick('ru')}>
+      <div
+        className={router.locale === 'ru' ? 'lang active' : 'lang'}
+        onClick={() => handleClick('ru')}
+      >
         Ру
       </div>
     </div>
-  )
+  );
 }
