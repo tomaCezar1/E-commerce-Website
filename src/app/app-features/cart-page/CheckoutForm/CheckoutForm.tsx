c55a904a486d821b4ba879aad50c2246dc43eb33
Mishanea <mihigh.ciobanu@gmail.com>
2021-01-19 13:06:30 +0200

    Fix disabled input

--- src/app/app-features/cart-page/CheckoutForm/CheckoutForm.tsx
+++ src/app/app-features/cart-page/CheckoutForm/CheckoutForm.tsx
@@ -184,6 +184,9 @@ export default function CheckoutForm({
               : 'checkout-form-input'
           }
         >
+          <option value="" disabled hidden>
+            {dictionary.callTime}
+          </option>
           <option value="9:00 - 12:00" className="checkout-option-text">
             9:00 - 12:00
           </option>
@@ -193,11 +196,7 @@ export default function CheckoutForm({
           <option value="15:00 - 18:00" className="checkout-option-text">
             15:00 - 18:00
           </option>
-          <option
-            value={dictionary.asap}
-            className="checkout-option-text"
-            selected
-          >
+          <option value={dictionary.asap} className="checkout-option-text">
             {dictionary.asap}
           </option>
         </Select>
